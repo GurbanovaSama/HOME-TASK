cc7c4c6aef6949652618479b0012090bdaad28e6
GurbanovaSama <samapg-ab205@code.edu.az>
2024-10-18 10:57:09 +0400

    task added

---
+++ GroupStudentClass/Group.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroupStudentClass
+{
+    internal class Group
+    {
+        public string group { get; }
+        public string shift { get; }
+        public Student[] students;
+        public int studentCount = 0;
+        public int maxStudent = 18;
+
+        public Group(string group, string shift, int v)
+        {
+            this.group = group;
+            this.shift = shift;
+            students = new Student[maxStudent];
+        }
+
+        public Group(string group, string shift) {
+            
+        }
+
+        public void AddStudent(Student student)
+        {
+            if (studentCount < maxStudent)
+            {
+                students[studentCount] = student;
+                studentCount++;
+                Console.WriteLine($"{student.Name} {student.Surname} qrupa elave edildi");
+            }
+            else
+            {
+                Console.WriteLine("Yerler dolub");
+            }
+
+        }
+
+
+        public Student FindStudent(string name)
+        {
+           foreach (Student student in students)
+            {
+                if (student.Name == name && student != null)
+                {
+                    return student;
+                }
+            }
+           return null;
+        }
+    }
+}
---
+++ GroupStudentClass/Student.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroupStudentClass
+{
+    internal class Student
+    {
+    
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Gender { get; set; }
+        public int Age { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Group { get; set; }
+        private int limit;
+        public int Limit {
+            get { return limit; }
+            set
+            {
+                if (value < 0)
+                {
+                    limit = 0;
+                }
+                else
+                {
+                    limit = value;
+                }
+            }
+        }
+   
+        public Student(string name, string surname , string gender, int age, string phoneNumber, int limit)
+        {
+            Name = name;
+            Surname = surname;
+            Gender = gender;
+            Age = age;
+            PhoneNumber = phoneNumber;
+            Limit = limit;
+          
+        }
+
+        public void Absent()
+        {
+            if (Limit > 0)
+            {
+                Limit--;
+                Console.WriteLine($"{Name} bu gun dersde istirak etmeyib");
+            }
+            else if (Limit == 0) 
+            {
+                Console.WriteLine($"{Name} kesilib");
+            }
+
+        }
+    }
+}
