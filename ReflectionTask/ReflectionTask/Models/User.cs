f84d3ec3d2eba04e3c7f891857f99aa156ade654
GurbanovaSama <samapg-ab205@code.edu.az>
2024-10-26 23:25:30 +0400

    task added

---
+++ ReflectionTask/ReflectionTask/Models/User.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReflectionTask.Models
+{
+    internal class User
+    {
+        private int id;
+        private string name;
+        private string surname;
+        private static int age;
+
+        public User(int id, string name, string surname)
+        {
+            this.id = id;
+            this.name = name;
+            this.surname = surname;
+        }
+
+        public void GetFullName()
+        {
+            Console.WriteLine($"{name} {surname}");
+        }
+
+        public static void ChangeAge(int modifiedAge)
+        {
+            age = modifiedAge;
+        }
+        public static int GetAge()
+        {
+            return age;
+        }
+
+        public override string ToString()
+        {
+            return $"{id}, {name}, {surname}, {age}"; 
+        }
+
+      
+    }
+}
---
+++ ReflectionTask/ReflectionTask/Program.cs
@@ -0,0 +1,38 @@
+﻿using ReflectionTask.Models;
+using System.Reflection;
+
+namespace ReflectionTask
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            User user = new(5, "Sama", "Gurbanova"); 
+            Type userType = typeof(User);
+
+            FieldInfo idField = userType.GetField("id", BindingFlags.Instance | BindingFlags.NonPublic);
+            idField.SetValue(user, 5);
+
+            FieldInfo nameField = userType.GetField("name", BindingFlags.NonPublic | BindingFlags.Instance);
+            nameField.SetValue(user, "Sama");
+
+            FieldInfo surnameField = userType.GetField("surname", BindingFlags.NonPublic | BindingFlags.Instance);
+            surnameField.SetValue(user, "Gurbanova");
+
+
+            user.GetFullName();
+
+            User.ChangeAge(19);
+            Console.WriteLine($"Age: {User.GetAge()}");
+
+            User.ChangeAge(20);
+            Console.WriteLine($"Modified Age: {User.GetAge()}");
+
+            Console.WriteLine(user.ToString());
+
+ 
+        }
+    }
+}
