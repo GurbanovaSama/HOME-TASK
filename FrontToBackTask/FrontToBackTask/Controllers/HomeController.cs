b135481992365a0b3cce87218de32ea8152a3cd8
GurbanovaSama <samapg-ab205@code.edu.az>
2024-11-21 10:17:29 +0400

    task added

---
+++ FrontToBackTask/FrontToBackTask/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using FrontToBackTask.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FrontToBackTask.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<User> users = new List<User>()
+            { 
+                new User() { Id = 1, Name = "Xalide" },
+                new User() { Id = 2, Name = "Xeyale" }
+            };
+            ViewData["users"] = "null";
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Work()
+        {
+            return View();
+        }
+        public IActionResult Pricing()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
